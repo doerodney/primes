51b4a0f655f1e24542d4f0f5c65b4dfba7bede37
doerodney <doerodney@gmail.com>
2024-03-01 19:26:28 -0500

    lua prime number filter finished.

--- lua/get_primes.lua
+++ lua/get_primes.lua
@@ -25,30 +25,38 @@ function is_prime(n)
 end
 
 --main-----------------------------------------------------------------------
---Get target count of primes argument:
-nprimes_target = tonumber(arg[1])
+function main()
+  --Get target count of primes argument:
+  nprimes_target = tonumber(arg[1])
 
---Define early primes:
-primes = {2, 3}
-nprimes_found = #primes
+  --Define early primes:
+  primes = {2, 3}
+  nprimes_found = #primes
 
-n = 5
-while nprimes_found < nprimes_target do
-  prime = is_prime(n) 
-  if prime then 
-    table.insert(primes, n)
-    nprimes_found = nprimes_found + 1
-  end
+  start = os.time()
+  n = 5
+  while nprimes_found < nprimes_target do
+    prime = is_prime(n) 
+    if prime then 
+      table.insert(primes, n)
+      nprimes_found = nprimes_found + 1
+    end
   
-  n = n + 2
+    n = n + 2
+  end
+
+  --Sum the primes:
+  sum = 0
+  for i = 1, #primes do
+    prime = primes[i]
+    sum = sum + prime
+    --print(prime)
+  end
+
+  now = os.time()
+  seconds = os.difftime(now, start)
+  print(string.format("The sum of the first %d prime numbers is %d", nprimes_target, sum))
+  print(string.format("Duration: %d [secs] (in lua)", seconds))
 end
 
---Sum the primes:
-sum = 0
-for i = 1, #primes do
-  prime = primes[i]
-  sum = sum + prime
-  --print(prime)
-end
-
-print(string.format("The sum of the first %d prime numbers is %d", nprimes_target, sum))
+main()
